f46c75c9240414edbcc5a7a682476142131c8d83
EyreFree <eyrefree@eyrefree.org>
2023-12-13 19:26:25 +0800

    fix

---
+++ framerusercontent.com/sites/1Us6su9NgAM2g7eoWHWtaA/chunk-DZMRIWE4.mjs
@@ -0,0 +1,2 @@
+function a(t,e){return{description:"A Conference of visionOS Creator around the world",robots:"max-image-preview:large",title:"Let's visionOS 2024"}}export{a};
+//# sourceMappingURL=chunk-DZMRIWE4.mjs.map
---
+++ framerusercontent.com/sites/1Us6su9NgAM2g7eoWHWtaA/chunk-XW2CFRGT.mjs
@@ -0,0 +1,2 @@
+import{Ca as r}from"./chunk-7JVHDLLH.mjs";r.loadFonts([]);var n=[],s=[".framer-iQ0PO .framer-styles-preset-i3m7js:not(.rich-text-wrapper), .framer-iQ0PO .framer-styles-preset-i3m7js.rich-text-wrapper a { --framer-link-current-text-decoration: underline; --framer-link-hover-text-decoration: underline; --framer-link-text-decoration: none; }"],i="framer-iQ0PO";export{n as a,s as b,i as c};
+//# sourceMappingURL=chunk-XW2CFRGT.mjs.map
